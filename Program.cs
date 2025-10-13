799348bfdfc8f15212eb5f55cf6267294a88f692
Maryan <maryan@nubulus.es>
2025-10-13 09:12:02 +0200

    Initial commit: project files

---
+++ Program.cs
@@ -0,0 +1,84 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.Identity.Web;
+using Portal.BackEnd;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services
+    .AddAuthentication(OpenIdConnectDefaults.AuthenticationScheme)
+    .AddMicrosoftIdentityWebApp(builder.Configuration.GetSection("AzureAd"));
+
+builder.Services.AddAuthorization(options =>
+{
+    options.FallbackPolicy = options.DefaultPolicy;
+});
+
+const string CorsPolicy = "AllowFrontend";
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(CorsPolicy, policy =>
+        policy
+            .WithOrigins("http://localhost:5173")
+            .AllowAnyHeader()
+            .AllowAnyMethod()
+            .AllowCredentials()
+            .WithExposedHeaders("X-User-Auth")
+    );
+});
+
+var app = builder.Build();
+
+app.UseRouting();
+app.UseCors(CorsPolicy);
+app.UseAuthentication();
+app.UseAuthorization();
+
+
+app.MapGet("/api/v1/auth/sign-in", () =>
+{
+    return Results.Redirect("/api/v1/auth/success");
+}).RequireAuthorization();
+
+app.MapGet("/api/v1/auth/success", (HttpContext context) =>
+{
+    User user = new(
+        Guid.NewGuid(),
+        context.User.Claims.FirstOrDefault(c => c.Type == "name")?.Value!,
+        context.User.Identities.FirstOrDefault()!.Name!
+    );
+
+    context.Response.Headers.Append("X-User-Auth", user.ToString());
+    return Results.Redirect("http://localhost:5173/private");
+}).RequireAuthorization();
+
+
+app.MapGet("/api/v1/auth/sign-out", async (HttpContext context) =>
+{
+    var returnUrl = context.Request.Query["returnUrl"].ToString();
+    if (string.IsNullOrWhiteSpace(returnUrl))
+    {
+        returnUrl = "http://localhost:5173/login";
+    }
+
+    await context.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+
+    var isAuth = context.User?.Identity?.IsAuthenticated ?? false;
+    if (!isAuth)
+    {
+        context.Response.Redirect(returnUrl);
+        return;
+    }
+
+    var props = new AuthenticationProperties { RedirectUri = returnUrl };
+    await context.SignOutAsync(OpenIdConnectDefaults.AuthenticationScheme, props);
+}).AllowAnonymous();
+
+app.MapGet("/api/v1/auth/sesion-is-valid", (HttpContext context) =>
+{
+    var isAuthenticated = context.User.Identity?.IsAuthenticated ?? false;
+    return isAuthenticated ? Results.Ok() : Results.Unauthorized();
+}).AllowAnonymous();
+
+app.Run();
---
+++ User.cs
@@ -0,0 +1,9 @@
+﻿namespace NUBULUS.AccountsAppsPortalBackEnd;
+
+public record User(Guid Id, string Username, string Email)
+{
+    public override string ToString()
+    {
+        return Convert.ToBase64String(System.Text.Encoding.UTF8.GetBytes($"{Id}|{Username}|{Email}"));
+    }
+}
