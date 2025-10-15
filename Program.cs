a49149578116a24a782d7845c661df85ab6fec87
Maryan <maryan@nubulus.es>
2025-10-15 12:05:40 +0200

    Eliminar el soporte para la autenticación de cookies y el endpoint de cierre de sesión personalizado.

--- Program.cs
+++ Program.cs
@@ -1,5 +1,3 @@
-using Microsoft.AspNetCore.Authentication.Cookies;
-using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 using Microsoft.Identity.Web;
 using NUBULUS.AccountsAppsPortalBackEnd.Infraestructure;
@@ -44,29 +42,5 @@ app.UseAuthorization();
 
 // Auth Endpoints
 app.MapSignInEndpoint().MapSignOutEndpoint().MapIsValidSessionEndpoint();
-
-
-
-app.MapGet("/api/v1/auth/sign-out", async (HttpContext context) =>
-{
-    var returnUrl = context.Request.Query["returnUrl"].ToString();
-    if (string.IsNullOrWhiteSpace(returnUrl))
-    {
-        returnUrl = "http://localhost:5173/";
-    }
-
-    await context.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
-
-    var isAuth = context.User?.Identity?.IsAuthenticated ?? false;
-    if (!isAuth)
-    {
-        context.Response.Redirect(returnUrl);
-        return;
-    }
-
-    var props = new AuthenticationProperties { RedirectUri = returnUrl };
-    await context.SignOutAsync(OpenIdConnectDefaults.AuthenticationScheme, props);
-}).AllowAnonymous();
-
 
 app.Run();
